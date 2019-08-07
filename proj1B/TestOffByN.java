ccee06dc91ad6d5ff2b993cf25f3da878bf0fcec
Jingyi Luo <jingyiluofd@gmail.com>
2019-08-06 23:02:16 -0700

    add test

--- proj1B/TestOffByN.java
+++ proj1B/TestOffByN.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class TestOffByN {
+    CharacterComparator OffByN = new OffByN(0);
+
+    // Your tests go here.
+    @Test
+    public void testEqualChars() {
+        assertFalse(OffByN.equalChars('b', 'a'));
+        assertFalse(OffByN.equalChars('c', 'D'));
+        assertTrue(OffByN.equalChars('d', 'd'));
+        assertTrue(OffByN.equalChars('%', '%'));
+        assertFalse(OffByN.equalChars('F', 'f'));
+    }
 }
