27f1a704abb7a29426f5512d39fddfc5cc4ae820
jingyiluofd <35879069+jingyiluofd@users.noreply.github.com>
2019-08-06 22:17:33 -0700

    Proj1 a (#4)
    * add file for proj0
    
    * project0 has been done!
    
    * suggested changes have been finished
    
    * removed the commented code
    
    * removed commented codes
    
    * no message
    
    * proj1A Linked List Deque created !
    
    * ArrayDeque created
    
    * unify the resize logic to an independent method

---
+++ proj1A/ArrayDeque.java
@@ -0,0 +1,203 @@
+/**
+ * This deque must use arrays as the core data structure.
+ *
+ * @param <T>
+ *
+ * @author ljy
+ */
+public class ArrayDeque<T> {
+    /**
+     * items size in array deque.
+     */
+    private int size;
+    /**
+     * index for the next item added to the front of the deque.
+     */
+    private int nextFirst;
+    /**
+     * index for the next item added to the back of the deque.
+     */
+    private int nextLast;
+    /**
+     * value of the array deque.
+     */
+    private T[] items;
+
+    /**
+     * an empty array deque.
+     */
+    public ArrayDeque() {
+        items = (T[]) new Object[8];
+        size = 0;
+        nextFirst = 0;
+        nextLast = 0;
+    }
+
+    /**
+     * a deep copy of other.
+     *
+     * @param other ArrayDeque
+     */
+    public ArrayDeque(ArrayDeque other) {
+        items = (T[]) new Object[other.items.length];
+        size = other.size;
+        nextFirst = other.nextFirst;
+        nextLast = other.nextLast;
+        System.arraycopy(other.items, 0, items, 0, other.items.length);
+    }
+
+    /**
+     * resize the array deque.
+     *
+     * @param capacity int
+     *
+     */
+    private void resize(int capacity) {
+        T[] a = (T[]) new Object[capacity];
+        int first;
+        if (nextFirst == items.length - 1) {
+            first = 0;
+        } else {
+            first = nextFirst + 1;
+        }
+        for (int i = 0; i < size; i++) {
+            a[i] = get(first);
+            first = first + 1;
+            if (first == items.length) {
+                first = 0;
+            }
+        }
+        items = a;
+        nextLast = size;
+        nextFirst = items.length - 1;
+    }
+
+    /**
+     * Adds an item of type T to the front of the deque.
+     * @param item T
+     */
+    public void addFirst(T item) {
+        if (size == items.length) {
+            resize(size * 4);
+        }
+        items[nextFirst] = item;
+        size = size + 1;
+        nextFirst = nextFirst - 1;
+        if (nextFirst == -1) {
+            nextFirst = items.length - 1;
+        }
+
+    }
+
+    /**
+     * Adds an item of type T to the back of the deque.
+     * @param item T
+     */
+    public void addLast(T item) {
+        if (size == items.length) {
+            resize(size * 4);
+        }
+        items[nextLast] = item;
+        size = size + 1;
+        nextLast = nextLast + 1;
+        if (nextLast == items.length) {
+            nextLast = 0;
+        }
+    }
+
+    /**
+     * Returns true if deque is empty, false otherwise.
+     * @return
+     */
+    public boolean isEmpty() {
+        return (size == 0);
+    }
+
+    /**
+     * Returns the number of items in the deque.
+     * @return
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Prints the items in the deque from first to last, separated by a space.
+     * Once all the items have been printed, print out a new line.
+     */
+    public void printDeque() {
+        int first;
+        if (nextFirst == items.length - 1) {
+            first = 0;
+        } else {
+            first = nextFirst + 1;
+        }
+        for (int i = 0; i < items.length; i++) {
+            System.out.print(items[first] + " ");
+            first = first + 1;
+            if (first == items.length) {
+                first = 0;
+            }
+        }
+        System.out.print("\n");
+
+    }
+
+    /**
+     * Reduce the size of array deque.
+     */
+    private void reduceSize() {
+        double doubleSize = size;
+        double val = doubleSize / items.length;
+        if ((items.length >= 16) && val < 0.25) {
+            resize(size * 4);
+        }
+    }
+
+    /**
+     * Removes and returns the item at the front of the deque.
+     * If no such item exists, returns null.
+     * @return
+     */
+    public T removeFirst() {
+        T returnValue;
+        size = size - 1;
+        nextFirst = nextFirst + 1;
+        if (nextFirst == items.length) {
+            nextFirst = 0;
+        }
+        returnValue = items[nextFirst];
+        items[nextFirst] = null;
+        reduceSize();
+        return returnValue;
+    }
+
+    /**
+     * Removes and returns the item at the back of the deque.
+     * If no such item exists, returns null.
+     * @return
+     */
+    public T removeLast() {
+        T returnValue;
+        size = size - 1;
+        nextLast = nextLast - 1;
+        if (nextLast == -1) {
+            nextLast = items.length - 1;
+        }
+        returnValue = items[nextLast];
+        items[nextLast] = null;
+        reduceSize();
+        return returnValue;
+    }
+
+    /**
+     * Gets the item at the given index,
+     * where 0 is the front, 1 is the next item, and so forth.
+     * If no such item exists, returns null. Must not alter the deque!
+     * @param index int
+     * @return
+     */
+    public T get(int index) {
+        return items[index];
+    }
+}
