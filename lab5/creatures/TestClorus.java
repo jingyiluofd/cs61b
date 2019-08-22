63498693c8cb576a85c1182c788bbe5b70c9b40b
Jingyi Luo <jingyiluofd@gmail.com>
2019-08-21 23:25:57 -0700

    no message

--- lab5/creatures/TestClorus.java
+++ lab5/creatures/TestClorus.java
@@ -1,4 +1,124 @@
 package creatures;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.awt.Color;
+import huglife.Direction;
+import huglife.Action;
+import huglife.Occupant;
+import huglife.Impassible;
+import huglife.Empty;
+
+/** Tests the plip class
+ *  @authr FIXME
+ */
 
 public class TestClorus {
+
+    @Test
+    public void testBasics() {
+        Clorus p = new Clorus(2);
+        Plip q = new Plip(1.5);
+        assertEquals(2, p.energy(), 0.01);
+        assertEquals(new Color(34, 0, 231), p.color());
+        p.move();
+        assertEquals(1.97, p.energy(), 0.01);
+        p.move();
+        assertEquals(1.94, p.energy(), 0.01);
+        p.stay();
+        assertEquals(1.93, p.energy(), 0.01);
+        p.stay();
+        assertEquals(1.92, p.energy(), 0.01);
+        p.attack(q);
+        assertEquals(3.42, p.energy(), 0.01);
+
+    }
+
+    @Test
+    public void testReplicate() {
+        Clorus p = new Clorus(1.5);
+        Clorus q = p.replicate();
+        assertNotEquals(p, q);
+        assertEquals(0.75, p.energy(), 0.01);
+        assertEquals(0.75, q.energy(), 0.01);
+    }
+
+    @Test
+    public void testChoose() {
+
+        // No empty adjacent spaces; stay.
+        Clorus p = new Clorus(1.2);
+        HashMap<Direction, Occupant> surrounded = new HashMap<Direction, Occupant>();
+        surrounded.put(Direction.TOP, new Impassible());
+        surrounded.put(Direction.BOTTOM, new Impassible());
+        surrounded.put(Direction.LEFT, new Impassible());
+        surrounded.put(Direction.RIGHT, new Impassible());
+
+        Action actual = p.chooseAction(surrounded);
+        Action expected = new Action(Action.ActionType.STAY);
+
+        assertEquals(expected, actual);
+
+        // any Plips are seen, the Clorus will ATTACK one of them randomly.
+        p = new Clorus(1.2);
+        HashMap<Direction, Occupant> topPlip = new HashMap<Direction, Occupant>();
+        topPlip.put(Direction.TOP, new Plip(1.5));
+        topPlip.put(Direction.BOTTOM, new Empty());
+        topPlip.put(Direction.LEFT, new Impassible());
+        topPlip.put(Direction.RIGHT, new Impassible());
+
+        actual = p.chooseAction(topPlip);
+        expected = new Action(Action.ActionType.ATTACK, Direction.TOP);
+
+        assertEquals(expected, actual);
+
+
+        // Energy >= 1; replicate towards an empty space.
+        p = new Clorus(1.2);
+        HashMap<Direction, Occupant> topEmpty = new HashMap<Direction, Occupant>();
+        topEmpty.put(Direction.TOP, new Empty());
+        topEmpty.put(Direction.BOTTOM, new Impassible());
+        topEmpty.put(Direction.LEFT, new Impassible());
+        topEmpty.put(Direction.RIGHT, new Impassible());
+
+        actual = p.chooseAction(topEmpty);
+        expected = new Action(Action.ActionType.REPLICATE, Direction.TOP);
+
+        assertEquals(expected, actual);
+
+
+        // Energy >= 1; replicate towards an empty space.
+        p = new Clorus(1.2);
+        HashMap<Direction, Occupant> allEmpty = new HashMap<Direction, Occupant>();
+        allEmpty.put(Direction.TOP, new Empty());
+        allEmpty.put(Direction.BOTTOM, new Empty());
+        allEmpty.put(Direction.LEFT, new Empty());
+        allEmpty.put(Direction.RIGHT, new Empty());
+
+        actual = p.chooseAction(allEmpty);
+        Action unexpected = new Action(Action.ActionType.STAY);
+
+        assertNotEquals(unexpected, actual);
+
+
+        //Otherwise, the Clorus will MOVE to a random empty square.
+        p = new Clorus(.99);
+
+        HashMap<Direction, Occupant> leftEmpty = new HashMap<Direction, Occupant>();
+        topEmpty.put(Direction.TOP, new Impassible());
+        topEmpty.put(Direction.BOTTOM, new Impassible());
+        topEmpty.put(Direction.LEFT, new Empty());
+        topEmpty.put(Direction.RIGHT, new Impassible());
+
+        actual = p.chooseAction(topEmpty);
+        expected = new Action(Action.ActionType.MOVE, Direction.LEFT);
+
+        assertEquals(expected, actual);
+
+
+        // We don't have Cloruses yet, so we can't test behavior for when they are nearby right now.
+    }
 }
+
